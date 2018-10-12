83dd15b32c5717f6e66ca1d356ec89a434bcad36
David Orme <david.orme@rallyhealth.com>
2018-10-11 22:10:28 -0500

    Fix tracking java/boot/bin/* in git and commit

---
+++ java/boot/bin/ParentClassLoader.java
@@ -0,0 +1,15 @@
+// From https://github.com/boot-clj/boot-bin/blob/master/src/boot/bin/ParentClassLoader.java
+
+package boot.bin;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+
+// Exists solely so we have a type to seal to prevent user code from
+// altering our top-level CL.
+public class ParentClassLoader extends URLClassLoader {
+    public ParentClassLoader(ClassLoader parent) {
+        super(new URL[0], parent); }
+
+    public void addURL(URL url) {
+	super.addURL(url); }}
